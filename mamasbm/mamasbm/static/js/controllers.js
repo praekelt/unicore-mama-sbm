14bd1ee0b40cb83fc081db5124a90da995a08852
Milton Madanda <madandat@gmail.com>
2014-07-04 14:46:38 +0200

    show uploaded messages

--- mamasbm/mamasbm/static/js/controllers.js
+++ mamasbm/mamasbm/static/js/controllers.js
@@ -12,31 +12,40 @@ profilesControllers.controller('ProfilesListCtrl', ['$scope', 'Profile', '$locat
 
 profilesControllers.controller(
     'ProfileEditCtrl',
-    ['$scope', '$routeParams', 'Profile', '$location', function ($scope, $routeParams, Profile, $location) {
-    if($routeParams.uuid != 'new'){
-        $scope.profile = Profile.get({uuid: $routeParams.uuid});
-    }else{
-        $scope.profile = new Profile();
-    }
-    $scope.send_days = [
-        {id: 0, name: 'Monday'},
-        {id: 1, name: 'Tuesday'},
-        {id: 2, name: 'Wednesday'},
-        {id: 3, name: 'Thursday'},
-        {id: 4, name: 'Friday'},
-        {id: 5, name: 'Saturday'},
-        {id: 6, name: 'Sunday'}
-    ];
-    $scope.save = function(profile){
-        profile.$save()
-               .then(function(profile){
-                    $location.path('/profiles');
-               });
-    };
-    $scope.add = function(profile){
-        profile.$add()
-               .then(function(profile){
-                    $location.path('/profiles');
-               });
-    };
+    ['$scope', '$routeParams', 'Profile', '$location', '$http', '$route',
+    function ($scope, $routeParams, Profile, $location, $http, $route) {
+        if($routeParams.uuid != 'new'){
+            $scope.profile = Profile.get({uuid: $routeParams.uuid});
+        }else{
+            $scope.profile = new Profile();
+        }
+        $scope.send_days = [
+            {id: 0, name: 'Monday'},
+            {id: 1, name: 'Tuesday'},
+            {id: 2, name: 'Wednesday'},
+            {id: 3, name: 'Thursday'},
+            {id: 4, name: 'Friday'},
+            {id: 5, name: 'Saturday'},
+            {id: 6, name: 'Sunday'}
+        ];
+        $scope.save = function(profile){
+            profile.$save()
+                   .then(function(profile){
+                        $location.path('/profiles');
+                   });
+        };
+        $scope.add = function(profile){
+            profile.$add()
+                   .then(function(profile){
+                        $location.path('/profiles');
+                   });
+        };
+        $scope.remove_message_profile = function(profile, msg_profile){
+            url = '/web/api/message_profiles.json?uuid=' + msg_profile.uuid;
+            $http.delete(url)
+                 .then(function(){
+                        $location.path('/profiles/' + profile.uuid);
+                        $route.reload();
+                   });
+        };
 }]);
