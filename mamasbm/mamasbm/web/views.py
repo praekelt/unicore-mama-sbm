c39b3a1056b1a42ceaf14bd8c009452b9c971bd2
Milton Madanda <madandat@gmail.com>
2014-07-16 17:01:06 +0200

    cleaned up web view

--- mamasbm/mamasbm/web/views.py
+++ mamasbm/mamasbm/web/views.py
@@ -1,23 +1,10 @@
 from pyramid.httpexceptions import HTTPNotFound
-from pyramid.response import Response
 from pyramid.view import view_config
-from sqlalchemy.exc import DBAPIError
-
-from mamasbm.models import (
-    DBSession,
-    MyModel,
-)
 
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(
-            'Pyramid is having a problem using your SQL database.',
-            content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'mamasbm'}
+    return {}
 
 
 @view_config(route_name='admin', renderer='templates/admin.pt')
